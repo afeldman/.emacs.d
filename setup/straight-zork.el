b103dc9beb1890fca0c48a3b8a328bd867c80df6
anton feldmann <anton.feldmann@lynq.tech>
2025-12-25 19:43:19 +0100

    feat: add straight.el configuration for Zork trilogy with keybindings

---
+++ setup/straight-zork.el
@@ -0,0 +1,107 @@
+;;; straight-zork.el --- straight.el configuration for Zork games -*- lexical-binding: t -*-
+
+;; Copyright (C) 2025 Anton Feldmann
+
+;; Author: Anton Feldmann
+;; URL: https://github.com/afeldman/zork-emacs
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "27.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;;; Commentary:
+
+;; Complete straight.el configuration for installing and running
+;; the Zork trilogy in Emacs. This setup includes:
+;;
+;; - straight.el bootstrap
+;; - emacs-zmachine (ZIL engine)
+;; - zork-emacs (Zork I, II, III games)
+;; - macOS Homebrew path configuration
+;; - use-package integration (optional)
+;;
+;; Usage:
+;;   Add to ~/.emacs.d/init.el:
+;;     (load "~/.emacs.d/setup/straight-zork.el")
+;;
+;; Then restart Emacs or evaluate the buffer.
+;; Start games with: M-x zork-play-game
+
+;;; Code:
+
+;; Bootstrap straight.el package manager
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; macOS Homebrew paths (ARM64 + Intel compatibility)
+(when (eq system-type 'darwin)
+  (add-to-list 'exec-path "/opt/homebrew/bin")      ;; Apple Silicon
+  (add-to-list 'exec-path "/usr/local/bin")         ;; Intel
+  (setenv "PATH" (concat (getenv "PATH") ":/opt/homebrew/bin:/usr/local/bin")))
+
+;; Install use-package for cleaner configuration (optional)
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+;;; ZIL Engine Installation
+
+(use-package emacs-zmachine
+  :straight (:type git
+             :host github
+             :repo "afeldman/emacs-zmachine"
+             :branch "master"
+             :files ("elisp/*.el" "emacs-zmachine-pkg.el" "README.md"))
+  :config
+  (message "emacs-zmachine (ZIL engine) loaded successfully."))
+
+;;; Zork Games Installation
+
+(use-package zork-emacs
+  :straight (:type git
+             :host github
+             :repo "afeldman/zork-emacs"
+             :branch "master"
+             :files ("elisp/*.el" "zork-emacs-pkg.el" "README.md"))
+  :commands (zork-play-game zork-i zork-ii zork-iii)
+  :config
+  (message "Zork trilogy loaded! Use M-x zork-play-game to start."))
+
+;;; Keybindings (optional)
+
+;; Quick launcher for Zork games
+(global-set-key (kbd "C-c z") 'zork-play-game)
+
+;; Individual game shortcuts
+(global-set-key (kbd "C-c z 1") 'zork-i)
+(global-set-key (kbd "C-c z 2") 'zork-ii)
+(global-set-key (kbd "C-c z 3") 'zork-iii)
+
+;;; Customization (optional)
+
+;; Automatically save Zork game state on exit
+;; (add-hook 'kill-emacs-hook
+;;           (lambda ()
+;;             (when (get-buffer "*Zork*")
+;;               (with-current-buffer "*Zork*"
+;;                 ;; Save game state logic here
+;;                 (message "Zork session saved.")))))
+
+;;; Package Update Commands
+
+;; Update ZIL engine:   M-: (straight-pull-package 'emacs-zmachine)
+;; Update Zork games:   M-: (straight-pull-package 'zork-emacs)
+;; Rebuild packages:    M-: (straight-rebuild-package 'zork-emacs)
+
+(provide 'straight-zork)
+
+;;; straight-zork.el ends here
